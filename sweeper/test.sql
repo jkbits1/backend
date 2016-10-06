bf5fd002ac76c928543412e56215db88e7086b2f
jkbits1 <jkbits1@users.noreply.github.com>
2016-10-06 04:00:03 +0100

    basic sweeper sql

--- sweeper/test.sql
+++ sweeper/test.sql
@@ -77,50 +77,76 @@ select MAX("CreatedTimeStamp") from stage.status_rider;
 
 delete from stage.status_rider;
 
-CREATE FUNCTION stage.createRiders() RETURNS timestamp AS $$
+DROP FUNCTION stage.create_riders();
+
+-- NOTE: all code in a function exists in a transaction.
+--       either it all succeeds or it is all rolled back
+CREATE FUNCTION stage.create_riders() RETURNS timestamp AS $$
 DECLARE
     tstamp timestamp := '2000-01-01';
 BEGIN
     RAISE NOTICE 'tstamp here is %', tstamp; 
 
+    -- get timestamp of unprocessed riders 
     IF EXISTS (select 1 from stage.status_rider) THEN
       select MAX("CreatedTimeStamp") into tstamp from stage.status_rider;
     ELSE 
       tstamp := '2010-01-01';
     END IF;
 
+    -- create intermediate table of timestamps, processed flag and driverId
     INSERT INTO 
       stage.status_rider ("CreatedTimeStamp")     
-    SELECT "CreatedTimeStamp" FROM stage.websubmission_rider 
-    WHERE "CreatedTimeStamp" > tstamp;
-
-    INSERT INTO 
-      nov2016.rider ("Name")     
     SELECT 
-      stage.websubmission_rider."RiderFirstName" 
+      "CreatedTimeStamp" FROM stage.websubmission_rider 
+    WHERE 
+      "CreatedTimeStamp" > tstamp;
+
+    -- create riders in nov2016 db
+    -- only insert riders in intermediate tables, and with status == 1
+    -- ?? timestamp to be creation of nov2016 row, or original submission ??
+    INSERT INTO 
+      nov2016.rider 
+        (
+        "RiderID", "Name", "Phone", "Email", "EmailValidated",
+        "State", "City", "Notes", "DataEntryPoint", "VulnerablePopulation",
+        "NeedsWheelChair", "Active"
+        )     
+    SELECT
+      stage.status_rider."RiderID",
+      concat_ws(' ', 
+                stage.websubmission_rider."RiderFirstName"::text, 
+                stage.websubmission_rider."RiderLastName"::text) 
+      ,
+      stage.websubmission_rider."RiderPhone",
+      stage.websubmission_rider."RiderEmail",
+      stage.websubmission_rider."RiderEmailValidated"::int::bit,
+
+      stage.websubmission_rider."RiderVotingState",
+      'city?',
+      'notes?',
+      'entry?',
+      stage.websubmission_rider."RiderIsVulnerable"::int::bit,
+
+      stage.websubmission_rider."WheelchairCount"::bit,
+      true::int::bit
     FROM 
       stage.websubmission_rider
     INNER JOIN 
       stage.status_rider 
     ON 
       (stage.websubmission_rider."CreatedTimeStamp" = stage.status_rider."CreatedTimeStamp") 
-    WHERE stage.websubmission_rider."CreatedTimeStamp" > tstamp;
+    WHERE 
+          stage.websubmission_rider."CreatedTimeStamp" > tstamp 
+      AND stage.status_rider.status = 1;
     
-
-UPDATW status PROCESSED FG:AG
-
-
-    --
-    -- Create a subblock
-    --
-    -- DECLARE
-    --     quantity integer := 80;
-    -- BEGIN
-    --     RAISE NOTICE 'Quantity here is %', quantity;  -- Prints 80
-    --     RAISE NOTICE 'Outer quantity here is %', outerblock.quantity;  -- Prints 50
-    -- END;
-
-    -- RAISE NOTICE 'Quantity here is %', quantity;  -- Prints 50
+    UPDATE 
+      stage.status_rider
+    SET
+      status = 100
+    WHERE
+          stage.status_rider."CreatedTimeStamp" > tstamp 
+      AND stage.status_rider.status = 1;
 
     -- RAISE EXCEPTION 'Nonexistent ID --> %', user_id
     --   USING HINT = 'Please check your user ID';
@@ -129,7 +155,12 @@ UPDATW status PROCESSED FG:AG
 END;
 $$ LANGUAGE plpgsql;
 
-select stage.createRiders();
-drop FUNCTION stage.createRiders();
+drop FUNCTION stage.create_riders();
 
-delete from nov2016.driver;
+delete from stage.status_rider;
+delete from nov2016.rider;
+
+select stage.createRiders();
+
+select * from stage.status_rider order by "CreatedTimeStamp";
+select * from nov2016.rider order by "CreatedTimestamp";
